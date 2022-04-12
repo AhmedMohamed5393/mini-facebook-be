4ea17dd92a82d58d2b40bfac44fcf8778ff6c398
Ahmed Mohamed <ahmedmohamedalex93@gmail.com>
2022-04-13 01:18:37 +0200

    fix error

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import * as env from "./environment";
 import { AppController } from './app.component';
-import { TemplateModule } from './app/user/user.module';
+import { UserModule } from './app/user/user.module';
 @Module({
     imports: [
-        TemplateModule,
+        UserModule,
     ],
     controllers: [AppController],
 })
